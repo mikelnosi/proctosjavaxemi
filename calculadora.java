d8a8089d1774d069e61c1d3aae3a332147794cb7
Jose Miguel <xemiruiz@hotmail.com>
2024-11-23 11:59:02 +0100

    calculadora sin bucle hecha con metodos, sumar, restar, multiplicar y dividir

--- calculadora.java
+++ calculadora.java
@@ -1,35 +1,68 @@
 import java.util.Scanner;
 
 public class calculadora {
+    private static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        double num1, num2, resultado = 0;
-        String operacion;
 
-        System.out.print("Introduce el numero a operar: ");
+        double num1 = introducirDouble();
 
-        num1 = teclado.nextDouble();
+        menu();
+        String operacion = introducirString();
 
-        System.out.print("\nElige la operacion a realizar: ");
-        System.out.print("\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n");
+        double num2 = introducirDouble();
 
-        operacion = teclado.next();
-
-        System.out.print("\nIntroduce el  segundo numero a operar: ");
-
-        num2 = teclado.nextDouble();
-
-        teclado.close();
-
-        resultado = switch1(operacion, num1, num2);
+        double resultado = switch1(operacion, num1, num2);
 
         System.out.print("\nEl resultado es: " + resultado);
+
+        teclado.close();
+    }
+
+    private static double introducirDouble() {
+        boolean valido = false;
+        double num = 0;
+
+        while (!valido) {
+            System.out.print("Introduce un numero: ");
+            if (teclado.hasNextDouble()) {
+                num = teclado.nextDouble();
+                valido = true;
+            } else {
+                System.out.println("El numero introducido no es valido");
+                teclado.next();
+            }
+        }
+        return num;
+    }
+
+    private static String introducirString() {
+        boolean valido = false;
+        String texto = "";
+
+        while (!valido) {
+            System.out.print("Introduce un texto que no este vacio: ");
+            texto = teclado.next();
+            if (!texto.trim().isEmpty()) {
+                valido = true;
+            } else {
+                System.out.println("El texto introducido no es valido");
+            }
+        }
+        return texto;
+    }
+
+    private static void menu() {
+        System.out.print("\nElige la operacion a realizar: ");
+        System.out.print("\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n");
     }
 
     private static double division(double a, double b) {
         double resultado = 0;
-        if (b != 0) resultado = a / b;
-        else System.out.println("No se puede dividir entre 0");
+        if (b != 0)
+            resultado = a / b;
+        else
+            System.out.println("No se puede dividir entre 0");
         return resultado;
     }
 
